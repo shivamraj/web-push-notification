3cb775d574d81a826c0fd9787df219d06d9d91fb
shivamraj <shivamrajput007@gmail.com>
2022-08-01 12:58:31 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 
 // Your web app's Firebase configuration
-
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+function dynamicallyLoadScript(url) {
+    var script = document.createElement("script");  // create a script DOM node
+    script.src = url;  // set its src to the provided URL
+    script.async = false;
+   
+    document.head.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+}
+dynamicallyLoadScript('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+dynamicallyLoadScript('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
 
 const firebaseConfig = {
