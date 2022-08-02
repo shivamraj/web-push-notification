55df9c22a2be72e86d8966edb87be30cf7c3c21a
shivamraj <shivamrajput007@gmail.com>
2022-08-02 12:44:38 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 // Your web app's Firebase configuration
-const loadDynamicScript = (url,callback) => {
+const loadDynamicScript = (url, callback) => {
   const existingScript = document.getElementById('scriptId');
 
   if (!existingScript) {
@@ -16,60 +16,54 @@ const loadDynamicScript = (url,callback) => {
 
   if (existingScript && callback) callback();
 };
-loadDynamicScript('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js',()=>{
-loadDynamicScript('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js', loadFile);
+loadDynamicScript('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js', () => {
+  loadDynamicScript('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js', loadFile);
 });
 
-    const firebaseConfig = {
-      apiKey: "AIzaSyBPE-loIzu11y2BbiM9Ie6QQoA0LMyQCG8",
-      authDomain: "push-notification-demo-638e7.firebaseapp.com",
-      projectId: "push-notification-demo-638e7",
-      storageBucket: "push-notification-demo-638e7.appspot.com",
-      messagingSenderId: "88844666505",
-      appId: "1:88844666505:web:580bc5ee8de570049d6858"
-    };
-    // For getting client token
-    var vapidKey = 'BF-nmrF3lsg4dlrLYMkEl_q-5rp6MC5iwf94sQfUHKXMPgpTTyFzkd3Vt9j0Bpnqun-GHBe_WDqOg6-XKx4P_AE'
+const firebaseConfig = {
+  apiKey: "AIzaSyBPE-loIzu11y2BbiM9Ie6QQoA0LMyQCG8",
+  authDomain: "push-notification-demo-638e7.firebaseapp.com",
+  projectId: "push-notification-demo-638e7",
+  storageBucket: "push-notification-demo-638e7.appspot.com",
+  messagingSenderId: "88844666505",
+  appId: "1:88844666505:web:580bc5ee8de570049d6858"
+};
+// For getting client token
+var vapidKey = 'BF-nmrF3lsg4dlrLYMkEl_q-5rp6MC5iwf94sQfUHKXMPgpTTyFzkd3Vt9j0Bpnqun-GHBe_WDqOg6-XKx4P_AE'
 
-    // Notification data
-    var notification = {
-      'title': 'Portugal vs. Denmark',
-      'body': '5 to 1',
-      'icon': 'firebase-logo.png',
-      'click_action': 'http://localhost:8081'
-    };
+// Notification data
+var notification = {
+  'title': 'Portugal vs. Denmark',
+  'body': '5 to 1',
+  'icon': 'firebase-logo.png',
+  'click_action': 'http://localhost:8081'
+};
 
-    // server key send in Authorization header
-    var serverkey = 'AAAAFK-OBok:APA91bFG8XKRet1FTR1dbjqBAntU9yX4DvtOfyQRb1CpOWyhoQKqDoeJnOpnJmef4WTIO9M8Peap3lucFnYTARtGss8e-j2MrGf-ImdESYBdcM8SXBmxDpiWfPdFjG2DBIbNTRtLi7UT'
-    var messaging;
+// server key send in Authorization header
+var serverkey = 'AAAAFK-OBok:APA91bFG8XKRet1FTR1dbjqBAntU9yX4DvtOfyQRb1CpOWyhoQKqDoeJnOpnJmef4WTIO9M8Peap3lucFnYTARtGss8e-j2MrGf-ImdESYBdcM8SXBmxDpiWfPdFjG2DBIbNTRtLi7UT'
+var messaging;
 
-function loadFile(){
+function loadFile() {
 
-    // Initialize Firebase
-    var app = firebase.initializeApp(firebaseConfig);
-    messaging = firebase.messaging();
-   messaging.onMessage(res => {
-    var notification1 = new Notification(res.notification.title, { body: res.notification.message, icon: res.notification.icon, subtitle: res.notification.subtitle});
+  // Initialize Firebase
+  var app = firebase.initializeApp(firebaseConfig);
+  messaging = firebase.messaging();
+  messaging.onMessage(res => {
+    var notification1 = new Notification(res.notification.title, { body: res.notification.message, icon: res.notification.icon, subtitle: res.notification.subtitle });
   })
 }
 
 
-function sendPushNotification(token) {
+function saveClientTokenOnServer(token) {
 
-  fetch('https://fcm.googleapis.com/fcm/send', {
-    'method': 'POST',
-    'headers': {
-      'Authorization': 'key=' + serverkey,
-      'Content-Type': 'application/json'
-    },
-    'body': JSON.stringify({
-      'notification': notification,
-      'to': token
-    })
+  fetch('http://thenotifiers.000webhostapp.com/put' + new URLSearchParams({
+    id: token,
+  }), {
+    'method': 'GET',
   }).then(function (response) {
-    console.log(response);
+    console.log("token save on backend", response);
   }).catch(function (error) {
-    console.error(error);
+    console.error("not saved", error);
   })
 }
 
@@ -77,38 +71,38 @@ function getClientToken() {
   messaging.getToken({ vapidKey }).then((token) => {
     console.log('token====', token)
     document.getElementById('token').innerHTML = token
-    // sendPushNotification(token);
+    saveClientTokenOnServer(token);
     //shopify-section-slideshow
   })
 }
 
 function registerServiceWorker() {
-   if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/web-push-notification/firebase-messaging-sw.js',{updateViaCache: 'none'})
-        .then((registration) => {
-          messaging.useServiceWorker(registration);
-          // request notification permission and get token
-          console.log('Registration successful, scope is:', registration.scope);
-          getClientToken();
-          //TODO: ask For Permission To Receive Notifications
-        }).catch(function (err) {
-          console.log('Service worker registration failed, error:', err);
-        });
-      }
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/web-push-notification/firebase-messaging-sw.js', { updateViaCache: 'none' })
+      .then((registration) => {
+        messaging.useServiceWorker(registration);
+        // request notification permission and get token
+        console.log('Registration successful, scope is:', registration.scope);
+        getClientToken();
+        //TODO: ask For Permission To Receive Notifications
+      }).catch(function (err) {
+        console.log('Service worker registration failed, error:', err);
+      });
+  }
 }
 
 function subscribePushNotification() {
- 
-    if (Notification.permission !== "granted") {
-      Notification.requestPermission().then((permission) => {
-        if (permission === 'granted') {
-          registerServiceWorker();
-        }
-      })
-    }
+
+  if (Notification.permission !== "granted") {
+    Notification.requestPermission().then((permission) => {
+      if (permission === 'granted') {
+        registerServiceWorker();
+      }
+    })
+  }
 }
 
-window.onload = ()=>{
-   subscribePushNotification();
+window.onload = () => {
+  subscribePushNotification();
 }
 
