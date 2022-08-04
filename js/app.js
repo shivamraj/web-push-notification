96be8ae4d1f46d7b0af927a7c84d821bb2414ee3
shivamraj <shivamrajput007@gmail.com>
2022-08-04 19:40:29 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -92,6 +92,13 @@ function registerServiceWorker() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/web-push-notification/firebase-messaging-sw.js', { updateViaCache: 'none' })
       .then((registration) => {
+       registration.pushManager.getSubscription().then(subscription => {
+        if (subscription) {
+            console.log("subscription====111",subscription)
+          return subscription;
+        }
+          console.log("subscription====222",subscription)
+       })
         messaging.useServiceWorker(registration);
         // request notification permission and get token
         console.log('Registration successful, registration is:', registration);
